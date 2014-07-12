abe21c5180cbcfa6be873f2d388ec634939b5003
Nadia Vu <winter.nga@gmail.com>
2014-07-12 03:32:40 +0000

    Initial commit

---
+++ javascript.js
@@ -0,0 +1,21 @@
+function add(number1, number2) {
+    sum = number1 + number2
+    return sum
+};
+
+function multiply(number1, number2) {
+    product = number1 * number2
+    return product
+};
+
+function divide(number1, number2) {
+    quotient = number1 / number2
+    return quotient
+};
+
+function subtract(number1, number2) {
+    difference = number1 - number2
+    return difference
+};
+
+
