a5d05db46305371b6d0311a3ad8d554f2dfdd10a
Joshua Paling <joshua.paling@gmail.com>
2014-07-12 14:11:37 +1000

    tie jquery to dom

--- javascript.js
+++ javascript.js
@@ -1,21 +1,34 @@
-function add(number1, number2) {
-    sum = number1 + number2
-    return sum
-};
+$('document').ready(function(){
+    alert('yay the document is ready!')
+    $('.calc_btn.num').click(function (){
+        var value = $(this).data('val');
+        alert('Yay we got a click on number ' + value);
+    });
 
-function multiply(number1, number2) {
-    product = number1 * number2
-    return product
-};
+    $('.calc_btn.operator').click(function (){
+        var value = $(this).data('val');
+        alert('Yay we got a click on operator ' + value);
+    });
+});
 
-function divide(number1, number2) {
-    quotient = number1 / number2
-    return quotient
-};
+// function add(number1, number2) {
+//     sum = number1 + number2
+//     return sum
+// };
 
-function subtract(number1, number2) {
-    difference = number1 - number2
-    return difference
-};
+// function multiply(number1, number2) {
+//     product = number1 * number2
+//     return product
+// };
+
+// function divide(number1, number2) {
+//     quotient = number1 / number2
+//     return quotient
+// };
+
+// function subtract(number1, number2) {
+//     difference = number1 - number2
+//     return difference
+// };
 
 
